a87ee119fd2668a328520b278b9c348efaca9edd
eyza-cod2 <kratas.tom@seznam.cz>
2020-03-24 11:45:39 +0100

    Package created

---
+++ spec/cod2-gsc-editor-package-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import Cod2GscEditorPackageView from '../lib/cod2-gsc-editor-package-view';
+
+describe('Cod2GscEditorPackageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
